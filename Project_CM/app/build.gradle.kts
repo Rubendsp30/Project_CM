f73a1a09378a9671aebac85898f7b7795dee74f1
Paulo Meira <75759802+Palmeira01@users.noreply.github.com>
2023-12-20 07:45:34 +0000

    Vaccines Screen created (com pouco sucesso :) ))

--- Project_CM/app/build.gradle.kts
+++ Project_CM/app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.room:room-runtime:2.6.1")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     annotationProcessor ("androidx.room:room-compiler:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
