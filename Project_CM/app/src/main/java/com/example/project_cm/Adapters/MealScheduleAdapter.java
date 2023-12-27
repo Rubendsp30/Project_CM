eb9075def173e295d7d97556f3ccccaf78ee83b4
Rubendsp30 <72309163+Rubendsp30@users.noreply.github.com>
2023-12-27 16:44:21 +0000

    Change some variable names

--- Project_CM/app/src/main/java/com/example/project_cm/Adapters/MealScheduleAdapter.java
+++ Project_CM/app/src/main/java/com/example/project_cm/Adapters/MealScheduleAdapter.java
@@ -4,30 +4,23 @@ package com.example.project_cm.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.project_cm.Device;
-import com.example.project_cm.Fragments.TreatPopUpFragment;
 import com.example.project_cm.MealSchedule;
 import com.example.project_cm.R;
-import com.example.project_cm.ViewModels.DeviceViewModel;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MealScheduleAdapter extends RecyclerView.Adapter<MealScheduleAdapter.MealScheduleViewHolder> {
 
-    List<MealSchedule> meals;
+    List<MealSchedule> mealScheduleList;
 
-    public MealScheduleAdapter(List<MealSchedule> meals) {
-        this.meals = meals;
+    public MealScheduleAdapter(List<MealSchedule> mealScheduleList) {
+        this.mealScheduleList = mealScheduleList;
     }
 
     @NonNull
@@ -39,7 +32,7 @@ public class MealScheduleAdapter extends RecyclerView.Adapter<MealScheduleAdapte
 
     @Override
     public void onBindViewHolder(@NonNull  MealScheduleViewHolder holder, int position) {
-        MealSchedule meal = meals.get(position);
+        MealSchedule meal = mealScheduleList.get(position);
 
         SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
         String formattedTime = timeFormat.format(meal.getMealTime());
@@ -56,19 +49,19 @@ public class MealScheduleAdapter extends RecyclerView.Adapter<MealScheduleAdapte
         public final TextView homeMealScheduleHour;
         public final TextView homeMealScheduleDate;
 
-        public MealScheduleViewHolder(@NonNull View noteView) {
-            super(noteView);
+        public MealScheduleViewHolder(@NonNull View homeMealView) {
+            super(homeMealView);
             // Initialize the TextViews for the title and body of the note.
-            this.homeMealScheduleHour = noteView.findViewById(R.id.homeMealScheduleHour);
-            this.homeMealScheduleDate = noteView.findViewById(R.id.homeMealScheduleDate);
+            this.homeMealScheduleHour = homeMealView.findViewById(R.id.homeMealScheduleHour);
+            this.homeMealScheduleDate = homeMealView.findViewById(R.id.homeMealScheduleDate);
         }
     }
 
     @Override
     public int getItemCount() {
 
-        if (meals != null) {
-            return meals.size();
+        if (mealScheduleList != null) {
+            return mealScheduleList.size();
         } else {
             return 0;
         }
